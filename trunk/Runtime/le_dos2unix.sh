1419bad884be29fff87c3d069c4a81c9c8d22fb7
eranif <eranif@b1f272c1-3a1e-0410-8d64-bdc0ffbdec4b>
2007-06-14 22:24:37 +0000

    Added conversion script from dos -> unix

---
+++ trunk/Runtime/le_dos2unix.sh
@@ -0,0 +1,33 @@
+files=`find . -name "*.project"`
+wsp_files=`find . -name "*.workspace"`
+
+##
+## fix workspace
+##
+for file in $wsp_files
+do
+	echo fixing file $file ...
+	## replace any windows \ with unix /
+	\rm  -f $file.tmp
+	sed 's/\\/\//g' $file > $file.tmp
+	mv $file.tmp $file
+done
+
+## 
+## fix project files
+##
+for file in $files
+do
+	echo fixing file $file ...
+	## replace any windows \ with unix /
+	\rm  -f $file.tmp
+	sed 's/\\/\//g' $file > $file.tmp
+	mv $file.tmp $file
+	
+	## replace dll extesion to .so 
+	sed 's/\.dll/\.so/g' $file > $file.tmp
+
+	mv $file.tmp $file
+done
+echo done
+
