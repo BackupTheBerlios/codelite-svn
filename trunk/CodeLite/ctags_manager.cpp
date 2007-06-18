f1e9210876578412abbee00d658b9f7bc350bcfa
eranif <eranif@b1f272c1-3a1e-0410-8d64-bdc0ffbdec4b>
2007-06-18 17:40:01 +0000

    Replace bin/ctags-le to ctags-le, this ,,means thats ctags-le must exist under your PATH env variable

--- trunk/CodeLite/ctags_manager.cpp
+++ trunk/CodeLite/ctags_manager.cpp
@@ -93,7 +93,7 @@ wxString CtagsOptions::ToString() const
 //------------------------------------------------------------------------------
 
 TagsManager::TagsManager() : wxEvtHandler()
-, m_ctagsPath(wxT("bin/ctags-le"))
+, m_ctagsPath(wxT("ctags-le"))
 , m_ctags(NULL)
 , m_localCtags(NULL)
 , m_parseComments(false)
@@ -562,7 +562,7 @@ void TagsManager::SetCtagsPath(const wxString& path)
 	// Make this call threadsafe
 	wxCriticalSectionLocker locker(m_cs);
 
-	m_ctagsPath = wxFileName(path, wxT("bin/ctags-le"));
+	m_ctagsPath = wxFileName(path, wxT("ctags-le"));
 }
 
 void TagsManager::OnCtagsEnd(wxProcessEvent& event)
