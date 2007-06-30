3fd0008fecbed32ca814c80aa2f9cd3f31f082d8
eranif <eranif@b1f272c1-3a1e-0410-8d64-bdc0ffbdec4b>
2007-06-30 08:00:24 +0000

    - initial changes for new CC - as a result, some areas of the CC will not work properly, so be patient....

--- trunk/LiteEditor/context_cpp.cpp
+++ trunk/LiteEditor/context_cpp.cpp
@@ -7,6 +7,7 @@
 #include "macros.h" 
 #include "wx/xrc/xmlres.h"
 #include "algorithm"
+#include "language.h"
 
 //Images initialization
 wxBitmap ContextCpp::m_classBmp = wxNullBitmap;
@@ -113,6 +114,16 @@ ContextBase *ContextCpp::NewInstance(LEditor *container){
 	return new ContextCpp(container);
 }
 
+void ContextCpp::OnDwellEnd(wxScintillaEvent &event)
+{
+	wxUnusedVar(event);
+}
+
+void ContextCpp::OnDwellStart(wxScintillaEvent &event)
+{
+	wxUnusedVar(event);
+}
+
 wxString ContextCpp::GetImageString(const TagEntry &entry)
 {
 	if(entry.GetKind() == wxT("class"))
@@ -217,18 +228,6 @@ bool ContextCpp::IsCommentOrString(long pos)
 			style == wxSCI_C_CHARACTER);
 }
 
-void ContextCpp::OnDwellEnd(wxScintillaEvent &WXUNUSED(event))
-{
-	// cancel hover tip
-	LEditor &rCtrl = GetCtrl();
-	if( m_tipKind == TipHover )
-	{
-		if( rCtrl.CallTipActive() )
-			rCtrl.CallTipCancel();
-		m_tipKind = TipNone;
-	}
-}
-
 void ContextCpp::CallTipCancel()
 {
 	LEditor &rCtrl = GetCtrl();
@@ -258,6 +257,7 @@ void ContextCpp::OnCallTipClick(wxScintillaEvent &event)
 // >>>>>>>>>>>>>>>>>>>>>>>> CodeCompletion API - START
 //=============================================================================
 
+//user pressed ., -> or ::
 void ContextCpp::CodeComplete()
 {
 	LEditor &rCtrl = GetCtrl();
@@ -312,7 +312,6 @@ void ContextCpp::CodeComplete()
 	int lcurlyPos  = rCtrl.FindString(wxT("{"), 0, false, rCtrl.GetCurrentPos());
 	int start;
 	TagEntry tag;
-	int line = 1;
 
 	semiColPos > lcurlyPos ? start = semiColPos : start = lcurlyPos;
 	if(start < 0){
@@ -320,121 +319,18 @@ void ContextCpp::CodeComplete()
 	}
 
 	wxString expr = rCtrl.GetTextRange(start, pos);
-
-	// Get the closest function to the current caret position, this will give us a
-	// good idea of the scope we are in it
-	if( TagsManagerST::Get()->FunctionByLine(rCtrl.LineFromPosition(start)+1, rCtrl.GetFileName().GetFullPath(), rCtrl.GetProjectName(), tag) )
-	{
-		line = tag.GetLine();
-	}
-
-	long scopeStartPos = rCtrl.PositionFromLine(line - 1/* wxScintilla counts line from zero */);
-	wxString scope = rCtrl.GetTextRange(scopeStartPos, end);
-	wxString scopeName = tag.GetScopeName();
+	
+	//get the current scope name
+	wxString text = rCtrl.GetTextRange(0, rCtrl.GetCurrentPos());
+	wxString scopeName = LanguageST::Get()->GetScopeName(text);
 
 	if( showFuncProto )
 	{
-		// display call tip with function prototype
-		m_ct = TagsManagerST::Get()->GetFunctionTip(expr, scope, scopeName);
-		if( m_ct->Count() > 0 )
-		{
-			// we have a match
-			rCtrl.CallTipShow(rCtrl.GetCurrentPos(), m_ct->Next());
-			m_tipKind = TipFuncProto;
-		}
+		//display function tooltip 
 	}
 	else
 	{
-		//--------------------------------------------------------------
-		// Get list of candidates if any and popup the Autucomplete box
-		//--------------------------------------------------------------
-
-		std::vector<TagEntry> candidates;
-		if( TagsManagerST::Get()->AutoCompleteCandidates(expr, scope, scopeName, candidates) )
-		{
-			/// Convert the vector to a string delimited
-			wxString list;
-			size_t i=0;
-			for(; i<candidates.size()-1; i++)
-			{
-				list.Append(candidates.at(i).GetName() + GetImageString(candidates.at(i)) + wxT("@"));
-			}
-
-			list.Append(candidates.at(i).GetName() + GetImageString(candidates.at(i)));
-			GetCtrl().AutoCompShow(0, list);
-		}
-	}
-}
-
-void ContextCpp::OnDwellStart(wxScintillaEvent &event)
-{
-	LEditor &rCtrl = GetCtrl();
-	// Handle dewell only if a project is opened
-	if( rCtrl.GetProjectName().IsEmpty())
-		return;
-
-	int start;
-	int line = 1;
-	long pos = event.GetPosition();
-	int  end = rCtrl.WordEndPosition(pos, true);
-	int word_start = rCtrl.WordStartPosition(pos, true);
-
-	// try to guess whether we are "standing" over a function
-	int foundPos = wxNOT_FOUND;
-	wxChar nextchar = rCtrl.NextChar( end, foundPos );
-	bool isFunc = nextchar == wxT('(') ? true : false;
-
-	// get the expression we are standing on it
-	if( IsCommentOrString( pos ) )
-		return;
-	
-	// get the token
-	wxString token = rCtrl.GetTextRange(word_start, end);
-
-	// Get a full expression
-	// We define an expression, by reading from pos and up until we find
-	// the first '{' , ';' or SOT
-	int semiColPos = rCtrl.FindString(wxT(";"), 0, false, end);
-	int lcurlyPos  = rCtrl.FindString(wxT("{"), 0, false, end);
-	semiColPos > lcurlyPos ? start = semiColPos : start = lcurlyPos;
-
-	//---------------------------------
-	// Get the scope name
-	//---------------------------------
-	
-	// Get the closest function to the current caret position, this will give us a
-	// good idea of the scope we are in it
-	TagEntry tag;
-	if( TagsManagerST::Get()->FunctionByLine(rCtrl.LineFromPosition(start)+1,	// scintilla are counting from zero, while ctags are counting from one
-		rCtrl.GetFileName().GetFullPath(), rCtrl.GetProjectName(), tag) )
-	{
-		line = tag.GetLine();
-	}
-
-	long scopeStartPos = rCtrl.PositionFromLine(line - 1/* wxScintilla counts line from zero */);
-	wxString scope = rCtrl.GetTextRange(scopeStartPos, end);
-	wxString scopeName = tag.GetScopeName();
-
-	//-------------------------------------------------------------
-	// now we are ready to process the scope and build our tips
-	//-------------------------------------------------------------
-	std::vector<wxString> tips;
-	TagsManagerST::Get()->GetHoverTip(token, scope, scopeName, isFunc, tips);
-
-	//-------------------------------------------------------------
-	// display a tooltip
-	//-------------------------------------------------------------
-	wxString tooltip;
-	if( tips.size() > 0 )
-	{
-		tooltip << tips[0];
-		for( size_t i=1; i<tips.size(); i++ )
-			tooltip << wxT("\n") << tips[i];
-
-		// cancel any old calltip and display the new one
-		rCtrl.CallTipCancel();
-		rCtrl.CallTipShow( event.GetPosition(), tooltip );
-		m_tipKind = TipHover;
+		//display list members
 	}
 }
 
